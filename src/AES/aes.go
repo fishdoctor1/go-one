2e22c4c0e3bc120266aee223b090c577fadea46d
TRUE\Kantasa <Kantasak_Sing@truecorp.co.th>
2022-07-04 18:28:05 +0700

    aes

---
+++ src/AES/aes.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"crypto/aes"
+	"encoding/hex"
+	"fmt"
+)
+
+func main() {
+	// cipher key
+	key := "thisis32bitlongpassphraseimusing"
+
+	// plaintext
+	pt := "This is a secret"
+
+	c := EncryptAES([]byte(key), pt)
+
+	// plaintext
+	fmt.Println(pt)
+
+	// ciphertext
+	fmt.Println(c)
+
+	// decrypt
+	DecryptAES([]byte(key), c)
+}
+
+func EncryptAES(key []byte, plaintext string) string {
+	// create cipher
+	c, err := aes.NewCipher(key)
+	CheckError(err)
+
+	// allocate space for ciphered data
+	out := make([]byte, len(plaintext))
+
+	// encrypt
+	c.Encrypt(out, []byte(plaintext))
+	// return hex string
+	return hex.EncodeToString(out)
+}
+
+func DecryptAES(key []byte, ct string) {
+	ciphertext, _ := hex.DecodeString(ct)
+
+	c, err := aes.NewCipher(key)
+	CheckError(err)
+
+	pt := make([]byte, len(ciphertext))
+	c.Decrypt(pt, ciphertext)
+
+	s := string(pt[:])
+	fmt.Println("DECRYPTED:", s)
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
